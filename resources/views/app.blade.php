bc71311cec103f2845edcc9937728fac74038c86
vinibalbino <silvavinicius2018@gmail.com>
2024-07-29 15:14:01 -0400

    refactor: Change favicon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
